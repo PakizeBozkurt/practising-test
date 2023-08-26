c6516118d38b69f9626aecb12fcf6876b220a12d
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-26 23:56:04 +0100

    success test

--- src/math.test.js
+++ src/math.test.js
@@ -3,11 +3,16 @@ import { add } from './math';
 
 it('should summarise all number values in an array', () => {
     // Arrange
-    const numbers = [1, 2, 3];
+    const numbers = [1, 2];
+    
     
     // Act
     const result = add(numbers);
     
     // Assert
-    expect(result).toBe(6);
+    const expectResult = numbers.reduce(
+      (preValue, curValue) => preValue + curValue,
+      0
+    );
+    expect(result).toBe(expectResult);
 });
