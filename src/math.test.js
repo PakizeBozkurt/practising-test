90e157c760d97ac0d4967a69ffbe186fcb9889ea
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-26 23:45:40 +0100

    AAA

--- src/math.test.js
+++ src/math.test.js
@@ -2,7 +2,10 @@ import { it, expect } from 'vitest';
 import { add } from './math';
 
 it('should summarise all number values in an array', () => {
+    // Arrange
     const numbers = [1, 2, 3];
+    
+
     const result = add(numbers);
     expect(result).toBe(6);
 });
