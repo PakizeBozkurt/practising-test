cc07c4abf3bf45ea87784dde3ab0a914cb24de5e
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-26 23:46:04 +0100

    AAA

--- src/math.test.js
+++ src/math.test.js
@@ -5,7 +5,8 @@ it('should summarise all number values in an array', () => {
     // Arrange
     const numbers = [1, 2, 3];
     
-
+    // Act
     const result = add(numbers);
+    
     expect(result).toBe(6);
 });
