7c63b95c2f2cc348fc54b91bdef7cb17bb8c6293
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-27 23:50:01 +0100

    testing

--- src/math.test.js
+++ src/math.test.js
@@ -1,19 +1,19 @@
-import { it, expect } from 'vitest';
-import { add } from './math';
+import { it, expect } from "vitest";
+import { add } from "./math";
 
-it('should summarise all number values in an array', () => {
-    // Arrange
-    const numbers = [1, 2, 3];
-    
-    
-    // Act
-    const result = add(numbers);
-    
-    // Assert
-    const expectResult = numbers.reduce(
+it("should summarise all number values in an array", () => {
+  
+  // Arrange
+  const numbers = [1, 2, 3];
 
-      (preValue, curValue) => preValue + curValue,
-      0
-    );
-    expect(result).toBe(expectResult);
+  // Act
+  const result = add(numbers);
+
+  // Assert
+  const expectResult = numbers.reduce(
+    (preValue, curValue) => preValue + curValue,
+    0
+  );
+
+  expect(result).toBe(expectResult);
 });
