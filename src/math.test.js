ae8bab3ad278545a977ab8eff7ca67a136099cf2
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-28 22:01:11 +0100

    hard code

--- src/math.test.js
+++ src/math.test.js
@@ -29,4 +29,10 @@ it('should yield NaN if a least one invalid number is provided', () => {
   expect(result).toBeNaN();
 });
 
-it('should yield a correct sum if an array of numeric string values is provided', () => {});
+it('should yield a correct sum if an array of numeric string values is provided', () => {
+  const numbers = ['1', '2'];
+
+  const result = add(numbers);
+
+  expect(result).toBe(3);
+});
