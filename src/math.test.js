c02e0529427f2377e237dfac1a2ac3428d02653b
Pakize Bozkurt <pakiozihak@gmail.com>
2023-08-30 23:15:56 +0100

    new test

--- src/math.test.js
+++ src/math.test.js
@@ -2,7 +2,6 @@ import { it, expect } from "vitest";
 import { add } from "./math";
 
 it("should summarise all number values in an array", () => {
-
   // Arrange
   const numbers = [1, 2];
 
@@ -18,26 +17,28 @@ it("should summarise all number values in an array", () => {
   expect(result).toBe(expectResult);
 });
 
-it('should yield NaN if a least one invalid number is provided', () => {
+it("should yield NaN if a least one invalid number is provided", () => {
   //Arrange
-  const inputs = ['invalid', 1];
+  const inputs = ["invalid", 1];
 
   //Act
   const result = add(inputs);
 
-  //Assert 
+  //Assert
   expect(result).toBeNaN();
 });
 
-it('should yield a correct sum if an array of numeric string values is provided', () => {
-  const numbers = ['1', '2'];
+it("should yield a correct sum if an array of numeric string values is provided", () => {
+  const numbers = ["1", "2"];
 
   const result = add(numbers);
 
-    const expectResult = numbers.reduce(
-      (preValue, curValue) => +preValue + +curValue,
-      0
-    );
+  const expectResult = numbers.reduce(
+    (preValue, curValue) => +preValue + +curValue,
+    0
+  );
 
-    expect(result).toBe(expectResult);
+  expect(result).toBe(expectResult);
 });
+
+it('should yield 0 if an empty array is provided', () => {});
