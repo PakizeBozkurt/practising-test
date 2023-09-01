df70ccc788eef5f75c4b388cd71ff67d8d291c39
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-01 23:03:28 +0100

    passed test

--- src/util/numbers.test.js
+++ src/util/numbers.test.js
@@ -1,7 +1,11 @@
-import { it, expext } from 'vitest';
+import { it, expext, expect } from 'vitest';
 
 import { transformToNumber } from './numbers';
 
 it('should transform a string number to a number of type number', () => {
-    
+    const input = '1';
+
+    const result = transformToNumber(input);
+
+    expect(result).toBeTypeOf('number');
 });
