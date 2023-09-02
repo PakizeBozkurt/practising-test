0ce17ec295525806f025d4549314f61521f050ff
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-02 23:15:48 +0100

    test passed

--- src/util/validation.test.js
+++ src/util/validation.test.js
@@ -3,6 +3,8 @@ import { it, expect } from 'vitest';
 import { validateNumber, validateStringNotEmpty } from './validation';
 
 it('should throw an error, if an empty string is provided', () => {
-    
+    const input = "";
+    const validationFn = () => validateStringNotEmpty(input);
+    expect(validationFn).toThrow();
 
 });
