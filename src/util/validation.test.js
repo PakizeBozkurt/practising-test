f0c149e69abe6946c1396088e3a9f62edfcdfddc
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-02 23:12:53 +0100

    empty string

---
+++ src/util/validation.test.js
@@ -0,0 +1,8 @@
+import { it, expect } from 'vitest';
+
+import { validateNumber, validateStringNotEmpty } from './validation';
+
+it('should throw an error, if an empty string is provided', () => {
+    
+
+});
