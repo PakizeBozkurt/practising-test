d6b0dab0f0927ed2041436c507a5d0d6654353de
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-04 21:13:21 +0100

    validateNumber

--- src/util/validation.test.js
+++ src/util/validation.test.js
@@ -21,4 +21,22 @@ describe("validationStringNotEmpty()", () => {
   });
 });
 
+describe("validateNumber()", () => {
+  it("should throw an error, if NaN is provided", () => {
+    const input = NaN;
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).toThrow();
+  });
 
+  it("should throw an error, if a non-numeric value is provided", () => {
+    const input = "1";
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).toThrow();
+  });
+
+  it("should throw an error, if a number is provided", () => {
+    const input = 1;
+    const validationFn = () => validateNumber(input);
+    expect(validationFn).not.toThrow();
+  });
+});
