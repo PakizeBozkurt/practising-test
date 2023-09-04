6c3da1e5ab88093c4a41237f79249c348275ec46
Pakize Bozkurt <pakiozihak@gmail.com>
2023-09-04 21:12:45 +0100

    validationStringNotEmpty

--- src/util/validation.test.js
+++ src/util/validation.test.js
@@ -1,23 +1,24 @@
-import { it, expect, describe } from 'vitest';
+import { it, expect, describe } from "vitest";
 
-import { validateNumber, validateStringNotEmpty } from './validation';
+import { validateNumber, validateStringNotEmpty } from "./validation";
 
-describe('validationStringNotEmpty()', () => {
-    it("should throw an error, if an empty string is provided", () => {
-      const input = "";
-      const validationFn = () => validateStringNotEmpty(input);
-      expect(validationFn).toThrow();
-    });
+describe("validationStringNotEmpty()", () => {
+  it("should throw an error, if an empty string is provided", () => {
+    const input = "";
+    const validationFn = () => validateStringNotEmpty(input);
+    expect(validationFn).toThrow();
+  });
 
-    it("should throw an error, if a non-numeric value is provided", () => {
-      const input = "1";
-      const validationFn = () => validateNumber(input);
-      expect(validationFn).toThrow();
-    });
+  it("should throw an error with a message that contains a reason (must not be empty)", () => {
+    const input = "";
+    const validationFn = () => validateStringNotEmpty(input);
+    expect(validationFn).toThrow(/must not be empty/);
+  });
+  it("should throw an error, if a  long string of blanks is provided", () => {
+    const input = "";
+    const validationFn = () => validateStringNotEmpty(input);
+    expect(validationFn).toThrow();
+  });
+});
 
-    it("should throw an error, if a number is provided", () => {
-      const input = 1;
-      const validationFn = () => validateNumber(input);
-      expect(validationFn).not.toThrow();
-    });
-})
+
